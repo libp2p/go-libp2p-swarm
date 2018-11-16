adaa228f563cb48e9cf15e50b1b2087b209928d6
Steven Allen <steven@stebalien.com>
2018-11-15 21:11:10 -0800

    warn when we encounter a useless transport

---
+++ transport_test.go
@@ -0,0 +1,47 @@
+package swarm_test
+
+import (
+	"context"
+	"testing"
+
+	swarmt "github.com/libp2p/go-libp2p-swarm/testing"
+
+	peer "github.com/libp2p/go-libp2p-peer"
+	transport "github.com/libp2p/go-libp2p-transport"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+type dummyTransport struct {
+	protocols []int
+	proxy     bool
+}
+
+func (dt *dummyTransport) Dial(ctx context.Context, raddr ma.Multiaddr, p peer.ID) (transport.Conn, error) {
+	panic("unimplemented")
+}
+
+func (dt *dummyTransport) CanDial(addr ma.Multiaddr) bool {
+	panic("unimplemented")
+}
+
+func (dt *dummyTransport) Listen(laddr ma.Multiaddr) (transport.Listener, error) {
+	panic("unimplemented")
+}
+
+func (dt *dummyTransport) Proxy() bool {
+	return dt.proxy
+}
+
+func (dt *dummyTransport) Protocols() []int {
+	return dt.protocols
+}
+
+func TestUselessTransport(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	swarm := swarmt.GenSwarm(t, ctx)
+	err := swarm.AddTransport(new(dummyTransport))
+	if err == nil {
+		t.Fatal("adding a transport that supports no protocols should have failed")
+	}
+}
