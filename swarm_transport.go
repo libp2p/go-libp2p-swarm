764a6103f5787921eb900beb35a77b68a58edf02
Jorropo <jorropo.pgm@gmail.com>
2020-02-29 10:31:02 +0100

    Fixing Quality calculation.

--- swarm_transport.go
+++ swarm_transport.go
@@ -1,18 +1,20 @@
 package swarm
 
 import (
+	"context"
 	"fmt"
 	"strings"
-	"context"
 
-		"github.com/libp2p/go-libp2p-core/transport"
-			"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/transport"
 
 	ma "github.com/multiformats/go-multiaddr"
+	manet "github.com/multiformats/go-multiaddr-net"
 )
 
 // 2^31
 const defaultNonProxyQuality = 2147483648
+
 // 2^31+2^30
 const defaultProxyQuality = 3221225472
 
@@ -73,11 +75,12 @@ func (s *Swarm) TransportForListening(a ma.Multiaddr) transport.QTransport {
 }
 
 var errTransportUncastable = fmt.Errorf("BaseTransport must be `transport.QTransport` or `transport.Transport`.")
+
 // AddTransport adds a transport to this swarm.
 //
 // Satisfies the Network interface from go-libp2p-transport.
-func (s *Swarm) AddTransport(ot transport.BaseTransport) error{
-var t transport.QTransport
+func (s *Swarm) AddTransport(ot transport.BaseTransport) error {
+	var t transport.QTransport
 	switch rt := ot.(type) {
 	case transport.QTransport:
 		t = rt
@@ -124,7 +127,7 @@ type transportUpgrader struct {
 	transport.BaseTransport
 }
 
-func (t transportUpgrader) Dial(ctx context.Context, raddr ma.Multiaddr, p peer.ID) (transport.QCapableConn, error){
+func (t transportUpgrader) Dial(ctx context.Context, raddr ma.Multiaddr, p peer.ID) (transport.QCapableConn, error) {
 	conn, err := t.BaseTransport.(transport.Transport).Dial(ctx, raddr, p)
 	if err != nil {
 		return nil, err
@@ -132,18 +135,49 @@ func (t transportUpgrader) Dial(ctx context.Context, raddr ma.Multiaddr, p peer.
 	return upgradedCapableConn{CapableConn: conn}, nil
 }
 
-func (t transportUpgrader) Score(_ ma.Multiaddr, _ peer.ID) (transport.Score, error) {
+func (t transportUpgrader) Score(raddr ma.Multiaddr, _ peer.ID) (transport.Score, error) {
+	if !t.CanDial(raddr) {
+		return transport.Score{}, fmt.Errorf("\"%s\" does not match %s.", raddr, t.BaseTransport)
+	}
 	if t.Proxy() {
+		if manet.IsIPLoopback(raddr) {
+			return transport.Score{
+				Quality:   defaultProxyQuality >> 16,
+				IsQuality: true,
+				Fd:        1,
+			}, nil
+		}
+		if manet.IsPrivateAddr(raddr) {
+			return transport.Score{
+				Quality:   defaultProxyQuality >> 8,
+				IsQuality: true,
+				Fd:        1,
+			}, nil
+		}
 		return transport.Score{
-			Quality: defaultProxyQuality,
+			Quality:   defaultProxyQuality,
 			IsQuality: true,
-			Fd:1,
+			Fd:        1,
+		}, nil
+	}
+	if manet.IsIPLoopback(raddr) {
+		return transport.Score{
+			Quality:   defaultNonProxyQuality >> 16,
+			IsQuality: true,
+			Fd:        1,
+		}, nil
+	}
+	if manet.IsPrivateAddr(raddr) {
+		return transport.Score{
+			Quality:   defaultNonProxyQuality >> 8,
+			IsQuality: true,
+			Fd:        1,
 		}, nil
 	}
 	return transport.Score{
-		Quality: defaultNonProxyQuality,
+		Quality:   defaultNonProxyQuality,
 		IsQuality: true,
-		Fd:1,
+		Fd:        1,
 	}, nil
 }
 
@@ -153,8 +187,21 @@ type upgradedCapableConn struct {
 }
 
 func (c upgradedCapableConn) Quality() uint32 {
+	raddr := c.RemoteMultiaddr()
 	if c.Transport().Proxy() {
+		if manet.IsIPLoopback(raddr) {
+			return defaultProxyQuality >> 16
+		}
+		if manet.IsPrivateAddr(raddr) {
+			return defaultProxyQuality >> 8
+		}
 		return defaultProxyQuality
+	}
+	if manet.IsIPLoopback(raddr) {
+		return defaultNonProxyQuality >> 16
+	}
+	if manet.IsPrivateAddr(raddr) {
+		return defaultNonProxyQuality >> 8
 	}
 	return defaultNonProxyQuality
 }
