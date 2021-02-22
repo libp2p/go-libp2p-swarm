3967aa81e86d1ffa529f5bd972a103427c164555
aarshkshah1992 <aarshkshah1992@gmail.com>
2021-02-22 15:05:05 +0530

    better logging

--- swarm_dial.go
+++ swarm_dial.go
@@ -252,9 +252,14 @@ func (s *Swarm) dialPeer(ctx context.Context, p peer.ID) (*Conn, error) {
 	defer log.EventBegin(ctx, "swarmDialAttemptSync", p).Done()
 
 	conn := s.bestConnToPeer(p)
+
 	forceDirect, _ := network.GetForceDirectDial(ctx)
 	if forceDirect {
+		fmt.Println("\n best conn to peer is", conn)
+		fmt.Println("\n all conns to peer are", s.ConnsToPeer(p))
+
 		if isDirectConn(conn) {
+			fmt.Printf("\n returning since we already have direct conn %v", conn)
 			return conn, nil
 		}
 	} else if conn != nil {
